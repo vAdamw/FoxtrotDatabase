21f85f23c8d4cb2ab341da8c36624400f81aac6a
ASahrlingHamm <anna.sahrling-hamm@edu.edugrade.se>
2022-12-22 13:46:28 +0100

    Kod

---
+++ src/main/java/com/example/foxtrotdatabases/Players/Players.java
@@ -0,0 +1,146 @@
+package com.example.foxtrotdatabases.Players;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "players")
+
+public class Players {
+    @Id
+    @Column(name = "player_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int playerID;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "nickname")
+    private String nickname;
+    @Column(name = "street_address")
+    private String streetAddress;
+    @Column(name = "postal_code")
+    private int postalCode;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "team_id")
+    private int teamID;
+
+    public Players() {
+    }
+
+    public Players(int playerID, String firstName, String lastName, String nickname,
+                   String streetAddress, int postalCode, String country, String email,
+                   int teamID) {
+        this.playerID = playerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.streetAddress = streetAddress;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.email = email;
+        this.teamID = teamID;
+    }
+
+    public Players(String firstName, String lastName, String nickname, String streetAddress,
+                   int postalCode, String country, String email, int teamID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.streetAddress = streetAddress;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.email = email;
+        this.teamID = teamID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    @Override
+    public String toString() {
+        return "Players{" +
+                "playerId=" + playerID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", streetAddress='" + streetAddress + '\'' +
+                ", postalCode=" + postalCode +
+                ", country='" + country + '\'' +
+                ", email='" + email + '\'' +
+                ", teamID=" + teamID +
+                '}';
+    }
+}
+
---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.foxtrotdatabases {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.persistence;
+    requires java.sql;
+    requires hibernate.entitymanager;
+    requires org.hibernate.orm.core;
+
+
+    opens com.example.foxtrotdatabases to javafx.fxml;
+    exports com.example.foxtrotdatabases;
+    exports com.example.foxtrotdatabases.Teams;
+    opens com.example.foxtrotdatabases.Teams to javafx.fxml;
+    exports com.example.foxtrotdatabases.Games;
+    opens com.example.foxtrotdatabases.Games to javafx.fxml;
+    exports com.example.foxtrotdatabases.Players;
+    opens com.example.foxtrotdatabases.Players to javafx.fxml, org.hibernate.orm.core;
+    exports com.example.foxtrotdatabases.Matches;
+    opens com.example.foxtrotdatabases.Matches to javafx.fxml;
+}
